2c64260ec653500b95675e6a5b43110aaac4907e
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2019-01-06 22:44:38 +0900

    add test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,43 +1,52 @@
-import { Application } from "probot";
 // Requiring our app implementation
-import myProbotApp from "../src";
+import fs from "fs";
+import { convertSource } from "../src/rulecheck";
+import { getTriggarableCode } from "../src/rulecheck";
 
-import issuesOpenedPayload from "./fixtures/issues.opened.json";
+const inputSourceToken = [
+  "self",
+  ".",
+  "assertFalse",
+  "(",
+  "a",
+  "not",
+  "in",
+  "b",
+  ")"];
+const expectedSourceToken = [
+  "self",
+  ".",
+  "assertNotIn",
+  "(",
+  "a",
+  ",",
+  "b",
+  ")"];
 
-test("that we can run tests", () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6);
+const pattern = [
+  "  self",
+  "  .",
+  "- assertFalse",
+  "+ assertNotIn",
+  "- not",
+  "- in",
+  "+ ,"];
+
+test("get pattern from source code", () => {
+  const patterns = fs.readFileSync("pattern_example.json").toString();
+  const code = getTriggarableCode(inputSourceToken, patterns);
+
+  if (code !== null) {
+    // expect(tokensConvertedDiff.convertedTokens).toEqual()
+    expect(code).toEqual(pattern);
+  }
 });
 
-describe("My Probot app", () => {
-  let app: Application;
-  let github: any;
+test("convert source code based on pattern", () => {
+  const tokensConvertedDiff = convertSource(inputSourceToken, pattern);
 
-  beforeEach(() => {
-    app = new Application();
-    // Initialize the app based on the code from index.ts
-    app.load(myProbotApp);
-    // This is an easy way to mock out the GitHub API
-    github = {
-      issues: {
-        createComment: jest.fn().mockReturnValue(Promise.resolve({})),
-      },
-    };
-    // Passes the mocked out GitHub API into out app instance
-    app.auth = () => Promise.resolve(github);
-  });
-
-  test("creates a comment when an issue is opened", async () => {
-    // Simulates delivery of an issues.opened webhook
-    await app.receive({
-      name: "issues.opened",
-      payload: issuesOpenedPayload,
-    });
-
-    // This test passes if the code in your index.ts file calls `context.github.issues.createComment`
-    expect(github.issues.createComment).toHaveBeenCalled();
-  });
+  if (tokensConvertedDiff !== null) {
+    // expect(tokensConvertedDiff.convertedTokens).toEqual()
+    expect(tokensConvertedDiff.convertedTokens).toEqual(expectedSourceToken);
+  }
 });
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
